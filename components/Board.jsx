53c1d2bc437dd6eddb5da5309f062895cacdbd49
Esteban <estebantoro.greenman@gmail.com>
2023-07-20 22:20:55 -0500

    Working board

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,16 +1,41 @@
 import Square from "./Square"
 import { useState } from "react";
 
-
 export default function Board() {
 
+
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+
     function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        if (xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
+
+    function handleReset() {
+        setSquares(Array(9).fill(null));
+    }
+
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = `Winner ${winner}!`
+    } else {
+        status = `Your turn ${(xIsNext ? 'X' : 'O')}!`
+    }
+
+
 
     return (
         <div
@@ -24,7 +49,10 @@ export default function Board() {
                 fontSize: '24px',
             }}
         >
+
             <p style={{ marginBottom: '10px' }}>TicTacToe</p>
+            <p>{status}</p>
+            <button onClick={handleReset}>Reset</button>
 
             <div className="row" >
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -43,4 +71,24 @@ export default function Board() {
             </div>
         </div>
     )
-} 
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
